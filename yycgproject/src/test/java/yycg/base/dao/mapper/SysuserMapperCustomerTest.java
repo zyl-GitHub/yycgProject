aad629b8b044de5a68cf8fec0c67835e906b86e4
zyl-GitHub <zyl_xupt@163.com>
2017-10-03 15:19:06 +0800

    Dao整合
    完成了spring有mybatis的整合

---
+++ yycgproject/src/test/java/yycg/base/dao/mapper/SysuserMapperCustomerTest.java
@@ -0,0 +1,35 @@
+package yycg.base.dao.mapper;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import yycg.base.pojo.po.Sysuser;
+import junit.framework.TestCase;
+
+public class SysuserMapperCustomerTest extends TestCase
+{
+	private ApplicationContext applicationContext;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		applicationContext = new ClassPathXmlApplicationContext(new String[]{
+				"spring/applicationContext.xml",
+				"spring/applicationContext-base-dao.xml"
+		});
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		// TODO 自动生成的方法存根
+		super.tearDown();
+	}
+	public void testFindSysuserById()
+	{
+		SysuserCustomMapper sysuserCustomMapper = (SysuserCustomMapper) applicationContext
+				.getBean("sysuserCustomMapper");
+		Sysuser sysuser =  sysuserCustomMapper.findSysuserById("286");
+		System.out.println(sysuser);
+	}
+}
